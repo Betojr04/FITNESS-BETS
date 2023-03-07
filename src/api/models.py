75384ed00eb6f3381c6fadc770eb24649b3f2744
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-03-07 18:42:50 +0000

    initiated repo, starting working on the models.py in the backend to start adding the different data models for all of the components of the application that will be used to funcion between users.

--- src/api/models.py
+++ src/api/models.py
@@ -4,8 +4,10 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    account_balance = db.relationship('AccountBalance', backref='user', useList=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
@@ -17,3 +19,16 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Watch_model(Model):
+    id = db.Column(db.Integer, primary_key)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    brand = db.Column(db.String(30), nullable=False)
+
+class Account_balance(db.Model):
+    id = db.Column(db.Integer, primary_key)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    balance = db.Column(db.Float)
+    currency = db.Column(db.String(3))
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
