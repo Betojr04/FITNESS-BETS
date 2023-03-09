7d419928056804fbcc819988593b3114bc0afb5a
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-03-09 18:35:30 +0000

    added navbar html and function for vertical navbar on the left side of the page

--- src/front/js/component/Navbar.js
+++ src/front/js/component/Navbar.js
@@ -10,7 +10,47 @@ export const Navbar = () => {
   const handleNavbarOpen = () => {
     setNavbarOpen(!navbarOpen);
   };
-  return <nav className="navbar"></nav>;
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">My App</div>
+
+      <button
+        className={`navbar-toggle ${navbarOpen ? "open" : ""}`}
+        onClick={handleNavbarToggle}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+
+      <ul className={`navbar-links ${navbarOpen ? "open" : ""}`}>
+        <li
+          className={`navbar-link ${activeLink === "Link 1" ? "active" : ""}`}
+          onClick={handleLinkClick}
+        >
+          Link 1
+        </li>
+        <li
+          className={`navbar-link ${activeLink === "Link 2" ? "active" : ""}`}
+          onClick={handleLinkClick}
+        >
+          Link 2
+        </li>
+        <li
+          className={`navbar-link ${activeLink === "Link 3" ? "active" : ""}`}
+          onClick={handleLinkClick}
+        >
+          Link 3
+        </li>
+        <li
+          className={`navbar-link ${activeLink === "Link 4" ? "active" : ""}`}
+          onClick={handleLinkClick}
+        >
+          Link 4
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
